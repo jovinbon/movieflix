e93568a1abed93ce3c8f8a98b1873295014d1935
jovinbon <jovinbon@hotmail.com>
2022-03-23 11:07:20 -0300

    Fix ReviewListing

--- frontweb/src/components/ReviewListing/index.tsx
+++ frontweb/src/components/ReviewListing/index.tsx
@@ -1,7 +1,4 @@
-import { AxiosRequestConfig } from 'axios';
-import { useEffect, useState } from 'react';
 import { Review } from 'types/review';
-import { BASE_URL, requestBackend } from 'util/requests';
 import './styles.css';
 
 type ListingReviews = {
@@ -9,24 +6,10 @@ type ListingReviews = {
 };
 
 const ReviewListing = ({ reviews }: ListingReviews) => {
-
-  const [list, setList] = useState<ListingReviews>();
-
-  useEffect(() => {
-    const config: AxiosRequestConfig = {
-      method: 'GET',
-      url: `${BASE_URL}/reviews`,
-      withCredentials: true,
-    };
-    requestBackend(config).then((response) => {
-      setList(response.data);
-    });
-  }, [reviews]);
-
   
   return (
     <div className="container my-4 catalog-container">
-      {list?.map(review =>(
+      {reviews?.map(review =>(
         <div className="content-reviews" key={review.id}>
           <h3>{review.user.name}</h3>
           <div className="review">
